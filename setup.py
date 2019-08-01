82b210522f6eaf8bbc8de605fab7549dbd58633e
Pratheek Rebala <pratheekrebala@gmail.com>
2019-08-01 17:00:09 -0400

    Pick correct platform when building
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
+import platform
+import shutil
+
+platform_name = platform.system()
+
+shutil.copyfile('PDFlib/bind/' + platform_name + '/tetlib_py.so', 'PDFlib/bind/tetlib_py.so')
+shutil.copyfile('PDFlib/bind/' + platform_name + '/tetlib_py2.so', 'PDFlib/bind/tetlib_py2.so')
+
+print("Building for " + platform_name)
 
 setup(
     name='PDFlib',
