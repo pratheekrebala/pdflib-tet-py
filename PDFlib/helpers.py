8318d6f96d0a682f3f68c4d7258a10ef2380bf05
Pratheek Rebala <pratheekrebala@gmail.com>
2021-05-18 11:40:11 -0400

    Packaging for correct shared libraries

--- PDFlib/helpers.py
+++ PDFlib/helpers.py
@@ -1,6 +1,6 @@
 from PDFlib.TET import TET
 from lxml import etree
-license_key = '***REMOVED***'
+license_key = 'L500902-010300-742594-KJZM72-NMYX52'
 
 class TETDocument:
     def __init__(self, file, doc_options, page_options):
